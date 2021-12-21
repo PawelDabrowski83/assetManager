ddfeb447a507f0852c9e70eba1df3dad3c9e18f3
pawel.d.dabrowski@gmail.com <pawel.d.dabrowski@gmail.com>
2021-12-21 21:43:06 +0100

    Add missing methods to Asset implementation.

--- src/main/java/pl/edu/pjwstk/assetManager/model/Asset.java
+++ src/main/java/pl/edu/pjwstk/assetManager/model/Asset.java
@@ -3,4 +3,5 @@ package pl.edu.pjwstk.assetManager.model;
 public interface Asset {
     void setType(AssetType type);
     AssetType getType();
+    int getId();
 }
--- src/main/java/pl/edu/pjwstk/assetManager/model/AssetImpl.java
+++ src/main/java/pl/edu/pjwstk/assetManager/model/AssetImpl.java
@@ -3,4 +3,19 @@ package pl.edu.pjwstk.assetManager.model;
 public class AssetImpl implements Asset {
     int id;
     AssetType type;
+
+    @Override
+    public void setType(AssetType type) {
+        this.type = type;
+    }
+
+    @Override
+    public AssetType getType() {
+        return type;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
 }
