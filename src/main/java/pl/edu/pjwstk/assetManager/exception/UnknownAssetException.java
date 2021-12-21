3fef077d4dff344aedb21040d1ce3cbabd2b79a9
pawel.d.dabrowski@gmail.com <pawel.d.dabrowski@gmail.com>
2021-12-21 21:19:36 +0100

    Prepare custom exception for AssetFactory.

---
+++ src/main/java/pl/edu/pjwstk/assetManager/exception/UnknownAssetException.java
@@ -0,0 +1,5 @@
+package pl.edu.pjwstk.assetManager.exception;
+
+public class UnknownAssetException extends IllegalArgumentException {
+
+}
--- src/main/java/pl/edu/pjwstk/assetManager/service/AssetFactory.java
+++ src/main/java/pl/edu/pjwstk/assetManager/service/AssetFactory.java
@@ -1,14 +1,27 @@
 package pl.edu.pjwstk.assetManager.service;
 
+import pl.edu.pjwstk.assetManager.exception.UnknownAssetException;
 import pl.edu.pjwstk.assetManager.model.Asset;
 import pl.edu.pjwstk.assetManager.model.AssetImpl;
 import pl.edu.pjwstk.assetManager.model.AssetType;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class AssetFactory {
 
-    public static Asset createAsset(AssetType type) {
+    public Asset createAsset(AssetType type) {
+        if (type == null) {
+            throw new UnknownAssetException();
+        }
         Asset asset = new AssetImpl();
         asset.setType(type);
         return asset;
     }
+
+    public Set<Asset> createAssets(int a, int b, int c) {
+        Set<Asset> assetSet = new HashSet<>();
+        return null;
+    }
+
 }
