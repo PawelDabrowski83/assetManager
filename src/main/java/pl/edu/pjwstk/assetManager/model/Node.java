9776fc732752cd8ca25c843d58d0f73a39d2a80c
pawel.d.dabrowski@gmail.com <pawel.d.dabrowski@gmail.com>
2021-12-26 13:26:02 +0100

    Add new fields to Node.

--- src/main/java/pl/edu/pjwstk/assetManager/model/Node.java
+++ src/main/java/pl/edu/pjwstk/assetManager/model/Node.java
@@ -2,6 +2,8 @@ package pl.edu.pjwstk.assetManager.model;
 
 import pl.edu.pjwstk.assetManager.service.AssetFactory;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,11 +11,19 @@ import java.util.Set;
 
 public class Node {
     static int count = 0;
-    int id;
+    final int id;
+    InetAddress ip;
+    final int port;
     Set<Asset> assets = new HashSet<>();
 
     public Node() {
         this.id = count++;
+        try {
+            this.ip = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        this.port = 20000 + id;
     }
 
     public static int getCount() {
@@ -22,10 +32,6 @@ public class Node {
 
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public Set<Asset> getAssets() {
